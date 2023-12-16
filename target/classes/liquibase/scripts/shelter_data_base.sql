8e86ac05d04954563eb1f3249bc961e4c05a1159
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-12-16 17:27:16 +0300

    Added an abstract class for working with user and volunteer data, a controller for volunteers with API documentation has also been added.

--- target/classes/liquibase/scripts/shelter_data_base.sql
+++ target/classes/liquibase/scripts/shelter_data_base.sql
@@ -3,11 +3,11 @@
 -- changeset john:1
 CREATE TABLE shelter (
     id BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    address TEXT NOT NULL,
-    work_time TEXT NOT NULL,
-    security_contact_details TEXT NOT NULL
-);
+       name TEXT NOT NULL,
+       address TEXT NOT NULL,
+       work_time TEXT NOT NULL,
+       security_contact_details TEXT NOT NULL
+    );
 
 CREATE TABLE volunteers (
     id BIGINT PRIMARY KEY,
