d6da39d18f4cdef4abd53f49eb96b9317954bb42
roma <rommur5@mail.ru>
2024-01-15 21:04:51 +0300

    пофиксил ошибку в отправке сообщений ботом о не сданном отчете

--- src/main/java/pro/sky/telegrambot/controller/ReportController.java
+++ src/main/java/pro/sky/telegrambot/controller/ReportController.java
@@ -1,24 +1,18 @@
 package pro.sky.telegrambot.controller;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import javassist.NotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pro.sky.telegrambot.model.Client;
 import pro.sky.telegrambot.model.Report;
-import pro.sky.telegrambot.model.Volunteer;
 import pro.sky.telegrambot.service.ReportService;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
